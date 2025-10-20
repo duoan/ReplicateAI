6c8cdece4551dc9fe8f3be94201824a65996392c
Victor An <2378740+duoan@users.noreply.github.com>
2025-10-19 23:09:37 -0700

    implement 5.3 mentioned optimizer scheduler, we call it as NoamScheduler

---
+++ stage2_representation/2017_AttentionIsAllYouNeed/src/optimizer.py
@@ -0,0 +1,15 @@
+from torch import optim
+
+
+class NoamScheduler(optim.lr_scheduler._LRScheduler):
+
+    def __init__(self, optimizer, d_model: int, warmup_steps: int= 4000, last_epoch=-1):
+        self.d_model = d_model
+        self.warmup_steps = warmup_steps
+        super().__init__(optimizer, last_epoch)
+
+
+    def get_lr(self):
+        step = max(1, self._step_count)
+        scale = (self.d_model ** -0.5)  * min(step ** -0.5, step * (self.warmup_steps ** -1.5))
+        return [base_lr * scale for base_lr in self.base_lrs]
